d38c18c53faef7bf1dddfa633b5c537a1c2493a6
Grégory Mounié <Gregory.Mounie@imag.fr>
2023-07-22 18:50:49 +0200

    Problème d'alignement dans le test
    Les unsigned long * doivent être alignés sur 8 bytes (x86-64
    bits) (problème levé par gcc-13 à l'exécution)

--- tests/test_mark.cc
+++ tests/test_mark.cc
@@ -1,5 +1,5 @@
 /*****************************************************
- * Copyright Grégory Mounié 2008-2013                *
+ * Copyright Grégory Mounié 2008-2023                *
  * This code is distributed under the GLPv3 licence. *
  * Ce code est distribué sous la licence GPLv3+.     *
  *****************************************************/
@@ -29,19 +29,19 @@ TEST( Mark, mmix ) {
     ASSERT_EQ(a0.size, 32UL);
     ASSERT_EQ(a0.ptr, tab);
 
-    void *pmem1 = mark_memarea_and_get_user_ptr(tab, 33, SMALL_KIND);
-    memset(pmem1, 0, 1);
+    void *pmem1 = mark_memarea_and_get_user_ptr(tab, 40, SMALL_KIND);
+    memset(pmem1, 0, 8);
 
     ASSERT_NE(pmem1, (void*)0);
     ASSERT_EQ(pmem1, & tab[2] );
     ASSERT_EQ(tab[1], knuth_mmix_one_round((unsigned long)tab) & ~(0b11UL));
-    ASSERT_EQ(tab[0], 33UL);
-    ASSERT_EQ(* (unsigned long *) (((char*)tab)+17),
-	      knuth_mmix_one_round((unsigned long)tab) & ~(0b11UL));
-    ASSERT_EQ(* (unsigned long *) (((char*)tab)+25), 33UL);
+    ASSERT_EQ(tab[0], 40UL);
+    ASSERT_EQ(tab[1], knuth_mmix_one_round((unsigned long)tab) & ~(0b11UL));
+    ASSERT_EQ(tab[3], knuth_mmix_one_round((unsigned long)tab) & ~(0b11UL));
+    ASSERT_EQ(tab[4], 40UL);
 
     Alloc a1 = mark_check_and_get_alloc(pmem1);
-    ASSERT_EQ(a1.size, 33UL);
+    ASSERT_EQ(a1.size, 40UL);
     ASSERT_EQ(a1.ptr, tab);
 
     void *pmem32 = mark_memarea_and_get_user_ptr(tab, 64, SMALL_KIND);
