e2f2fe3f6ce9c425740e29858b3f1c7d9f00a508
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2024-10-09 08:54:34 +0200

    Adding first version of mem_small malloc

--- src/mem_small.c
+++ src/mem_small.c
@@ -4,17 +4,79 @@
  * Ce code est distribué sous la licence GPLv3+.      *
  ******************************************************/
 
-#include <assert.h>
 #include "mem.h"
 #include "mem_internals.h"
+#include <assert.h>
 
-void *
-emalloc_small(unsigned long size)
-{
-    /* ecrire votre code ici */
-    return (void *)0;
+// typedef struct _MemArena {
+//     void *chunkpool;
+//     void *TZL[TZL_SIZE];
+//     int small_next_exponant;
+//     int medium_next_exponant;
+// } MemArena;
+//
+// unsigned long mem_realloc_small() {
+//   assert(arena.chunkpool == 0);
+//   unsigned long size = (FIRST_ALLOC_SMALL << arena.small_next_exponant);
+//   arena.chunkpool = mmap(0, size, PROT_READ | PROT_WRITE | PROT_EXEC,
+//                          MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+//   if (arena.chunkpool == MAP_FAILED)
+//     handle_fatalError("small realloc");
+//   arena.small_next_exponant++;
+//   return size;
+// }
+void *emalloc_small(unsigned long size) {
+  /* ecrire votre code ici */
+  // sizeof(unsigned long) == 8
+  // CHUNKSIZE = 96
+  printf("\nentering to emalloc_small\n");
+  unsigned long *tmpArenaChunkpool = (unsigned long *)arena.chunkpool;
+  unsigned long *freeNode;
+  if (arena.chunkpool != NULL) {
+    printf("chunk is not empty");
+    freeNode = tmpArenaChunkpool;
+    arena.chunkpool = tmpArenaChunkpool + CHUNKSIZE / sizeof(unsigned long *);
+    return (unsigned long *)mark_memarea_and_get_user_ptr(freeNode, CHUNKSIZE,
+                                                          SMALL_KIND);
+  } else {
+    unsigned long bloc = mem_realloc_small();
+    unsigned long **tmpArenaChunkpool = (unsigned long **)arena.chunkpool;
+
+    unsigned long **HeadTmpArenaChunkpool = tmpArenaChunkpool + 12;
+    unsigned long **antHeadTmpArenaChunkpool = tmpArenaChunkpool;
+    for (int i = 0; i < bloc / CHUNKSIZE; i += 12) {
+      *antHeadTmpArenaChunkpool = (unsigned long *)HeadTmpArenaChunkpool;
+      antHeadTmpArenaChunkpool += i;
+      HeadTmpArenaChunkpool += i;
+    }
+    return antHeadTmpArenaChunkpool;
+    // int i = 0;
+    // int counter = 0;
+    // while (tmpArenaChunkpool != NULL && counter <= bloc / 12) {
+    //   tmpArenaChunkpool += 12;
+    //   counter++;
+    // }
+    // while ((unsigned long *)arena.chunkpool != NULL) {
+    //   (unsigned long *)arena.chunkpool =
+    //       ((unsigned long *)arena.chunkpool) + 12;
+    //   counter++;
+    // }
+    // if (((unsigned long *)arena.chunkpool + 12) == NULL) {
+    //   printf("here is deborded chunkpool. ");
+    // }
+    // printf("\ncounter: %d\n", counter);
+    // for (; i < bloc / CHUNKSIZE; i += CHUNKSIZE) {
+    //   ;
+    // }
+    // printf("\n number of chunks %d\n", i);
+    // return (void *)0;
+  }
 }
 
 void efree_small(Alloc a) {
-    /* ecrire votre code ici */
+  /* ecrire votre code ici */
+  if (arena.chunkpool != NULL) {
+    a.ptr = arena.chunkpool;
+    arena.chunkpool = a.ptr;
+  }
 }
--- test.py
+++ test.py
@@ -2,7 +2,11 @@
 # address 0x5f4d059142a1 p+1:
 # address 0x5f4d059142a2 p+2:
 
-tab = 0x7108523c0000
-tmpPtr =0x7108523c0020
-# tmpPtr passed for 32 bytes 
-print(tmpPtr-tab)
+# tab = 0x7108523c0000
+# tmpPtr =0x7108523c0020
+
+# tmpPtr passed for 32 bytes
+# print(tmpPtr-tab)
+# so when is executed the mmap i have a new chunk of memory of 96bytes for 128
+print((96 << 7) / 96)
+print(0x7ffff7fb5060 - 0x7ffff7fb5000)
