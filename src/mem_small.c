cc26a971563763463bb10df336d7a6956791716b
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2024-10-12 17:16:03 +0200

    Adding just an stupid assert just to be clear what should be expected

--- src/mem_small.c
+++ src/mem_small.c
@@ -7,6 +7,7 @@
 #include "mem.h"
 #include "mem_internals.h"
 #include <assert.h>
+#define ULONGSTEP (CHUNKSIZE / (sizeof(unsigned long)))
 
 void *emalloc_small(unsigned long size) {
   if (arena.chunkpool == NULL) {
@@ -15,15 +16,20 @@ void *emalloc_small(unsigned long size) {
     unsigned long bloc = mem_realloc_small();
     unsigned long **tmpHead = arena.chunkpool;
     for (int i = 0; i < (bloc / CHUNKSIZE); i++) {
-      *tmpHead = (unsigned long *)tmpHead + 12;
-      tmpHead += 12;
+      *tmpHead = (unsigned long *)tmpHead + ULONGSTEP;
+      tmpHead += ULONGSTEP;
     }
     // 1288 / 96 == 128
     //(x + 96 )*128
     //  here the last element has to be in the pos
     //(x + 96 )*128
     //  x == address of arena.chunkpool + bloc
-    assert((unsigned long **)arena.chunkpool + 12 * 128 == tmpHead);
+    printf("\nvalue should be : %lu \n",
+           (unsigned long)arena.chunkpool + (unsigned long)(96 * 128));
+    printf("\nULONGSTEP: %lu \n", ULONGSTEP);
+
+    assert((unsigned long **)arena.chunkpool + ULONGSTEP * bloc / CHUNKSIZE ==
+           tmpHead);
   }
   void **firstNode = arena.chunkpool;
   arena.chunkpool = *(void **)firstNode;
